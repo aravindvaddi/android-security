db80ab49e29779cd2c86bfc7e897d232002382fd
Aravind Vaddi <aravindvaddi.github@gmail.com>
2020-12-18 17:52:59 -0500

    Add Part 3 - Encrypt all the REST API calls

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
@@ -45,7 +45,7 @@ class SecondFragment : Fragment() {
             var password : String = view.findViewById<EditText>(R.id.registerPassword).text.toString()
             var password2 : String = view.findViewById<EditText>(R.id.registerConfirmPassword).text.toString()
 
-            var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("http://appsecclass.report").addConverterFactory(GsonConverterFactory.create())
+            var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsecclass.report").addConverterFactory(GsonConverterFactory.create())
             var retrofit: Retrofit = builder.build()
             var client: UserInterface = retrofit.create(UserInterface::class.java)
             var loggedInUser: User? = null;
