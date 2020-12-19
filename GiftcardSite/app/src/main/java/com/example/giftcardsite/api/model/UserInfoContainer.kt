4d098c004046d9ce36ce6baa2bdcd90065106084
Aravind Vaddi <aravindvaddi.github@gmail.com>
2020-12-18 19:28:49 -0500

    Part 5: Remove all metric collecting code, all areas that needlessly interact with sensors, and all permissions that are not needed for the basic functionality of the application

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/UserInfoContainer.kt
+++
@@ -1,6 +0,0 @@
-package com.example.giftcardsite.api.model
-
-import android.location.Location
-import retrofit2.http.Header
-
-class UserInfoContainer(var location: Location?, var sensorData: String?, val token: String?)
